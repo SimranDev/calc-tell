29f4867a188eefe5ed00eaeca4bd9eabb6a69573
SimranDev <simran.develop@gmail.com>
2021-07-26 02:25:45 +1200

    refactor: unused imports removed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,7 @@
-import { FC, useState } from "react";
-import Button from "../components/Button/ButtonRadioType";
 import styled from "styled-components";
 import Header from "../components/Header/Header";
-import Label from "../components/Label/Label";
-import InputField from "../components/InputField/InputField";
 import Output from "../components/Output/Output";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  otherIncomeChange,
-  otherIncomePeriodChange,
-  addOtherIncome,
-  addLoan,
-  loanChange,
-  deleteLoan,
-  deleteAllLoan,
-  depositChange,
-  addCredit,
-  deleteCredit,
-  deleteAllCredit,
-  creditChange,
-} from "../store/store";
-import { RootState } from "../store/store";
-import OtherField from "../components/InputField/OtherIncomeField";
-import LoanField from "../components/InputField/LoanField";
-import DepositField from "../components/InputField/DepositField";
 import { DEFAULTS } from "../consts/styles";
-import AddButton from "../components/Button/AddButton";
 import SalaryBlock from "../combined-components/SalaryBlock";
 import IncomeBlock from "../combined-components/IncomeBlock";
 import LoanBlock from "../combined-components/LoanBlock";
@@ -34,9 +10,7 @@ import DepositBlock from "../combined-components/DepositBlock";
 
 export interface HomeProps {}
 
-const Home: FC<HomeProps> = () => {
-  const dispatch = useDispatch();
-
+const Home: React.FC<HomeProps> = () => {
   return (
     <Container>
       <Content>
