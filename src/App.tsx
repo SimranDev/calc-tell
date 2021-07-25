3d0688d23fd5cf67d5a769c84e062e6c932e208f
SimranDev <simran.develop@gmail.com>
2021-07-26 02:12:05 +1200

    feat: footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Home />
+      <Footer />
     </>
   );
 }
