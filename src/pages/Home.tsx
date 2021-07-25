582abf154fedf664fc50361f768ca0bc3925a77a
SimranDev <simran.develop@gmail.com>
2021-07-25 13:49:17 +1200

    feat: all components added and linked with state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,46 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Button from "../components/Button/Button";
 import styled from "styled-components";
 import Header from "../components/Header/Header";
 import Label from "../components/Label/Label";
 import InputField from "../components/InputField/InputField";
 import Output from "../components/Output/Output";
-import { useDispatch } from "react-redux";
-import { increment } from "../store/store";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  salaryChange,
+  salary2Change,
+  salaryPeriodChange,
+  salary2PeriodChange,
+  otherIncomeChange,
+  otherIncomePeriodChange,
+  addOther,
+  deleteOther,
+  deleteAllOther,
+  addLoan,
+  loanChange,
+  deleteLoan,
+  deleteAllLoan,
+  depositChange,
+  addCredit,
+  deleteCredit,
+  deleteAllCredit,
+  creditChange,
+} from "../store/store";
+import { RootState } from "../store/store";
+import OtherField from "../components/InputField/OtherField";
+import LoanField from "../components/InputField/LoanField";
+import DepositField from "../components/InputField/DepositField";
+import { DEFAULTS } from "../consts/styles";
 
 export interface HomeProps {}
 
 const Home: FC<HomeProps> = () => {
   const dispatch = useDispatch();
+  const { other_list, loan_list, credit_list } = useSelector(
+    (state: RootState) => state.counter
+  );
+
+  const [mode, setMode] = useState(1);
 
   return (
     <Container>
@@ -19,34 +48,190 @@ const Home: FC<HomeProps> = () => {
         <Header />
         <section>
           <FormContainer>
+            {/* ------Home many buyying------ */}
             <Label text="How many of you are buying the property?" />
             <span>
               <Button
                 title="Just Me"
-                name="option1"
-                onClick={() => dispatch(increment())}
+                name="radios1"
+                onClick={() => {
+                  setMode(1);
+                  dispatch(salary2Change(0));
+                }}
+                checked
+                style={{ marginRight: DEFAULTS.btnSpacing }}
               />
               <Button
-                title="Just Me 2"
-                name="option1"
-                onClick={() => dispatch(increment())}
+                title="I'm buing with someone"
+                name="radios1"
+                onClick={() => {
+                  setMode(2);
+                  dispatch(salary2Change(0));
+                }}
               />
-              {/* <Button title="I'm buying with someone" name="option1" /> */}
-            </span>
-            <Label text="How many of you are buying the property?" />
-            <button onClick={() => dispatch(increment())}>TESTING</button>
-
-            <span>
-              {/* <Button title="X" name="option2" />
-              <Button title="Y" name="option2" checked /> */}
             </span>
             <Label text="What's your base salary/wages? (before tax)" />
+            <InputField
+              onChange={(val) => dispatch(salaryChange(Number(val)))}
+              onPeriodChange={(val) =>
+                dispatch(salaryPeriodChange(Number(val)))
+              }
+            />
+            {mode === 2 && (
+              <>
+                <Label text="What's the second applicant's base salary/wages? (before tax)" />
+                <InputField
+                  onChange={(val) => dispatch(salary2Change(Number(val)))}
+                  onPeriodChange={(val) =>
+                    dispatch(salary2PeriodChange(Number(val)))
+                  }
+                />
+              </>
+            )}
+
+            {/* ------Another source of income------ */}
+            <Label text="Do you have another source of income?" />
             <span>
-              <InputField />
+              <Button
+                title="Yes"
+                name="radios2"
+                onClick={() => {
+                  dispatch(addOther());
+                }}
+                style={{ marginRight: DEFAULTS.btnSpacing }}
+              />
+              <Button
+                title="No"
+                name="radios2"
+                onClick={() => {
+                  dispatch(deleteAllOther());
+                }}
+                checked
+              />
+            </span>
+            {other_list.map((row, index) => (
+              <div key={index}>
+                <Label text={"Other income #" + (index + 1)} />
+                <span>
+                  <OtherField
+                    onChange={(value: string) =>
+                      dispatch(
+                        otherIncomeChange({ index, value: Number(value) })
+                      )
+                    }
+                    onPeriodChange={(value: number) =>
+                      dispatch(
+                        otherIncomePeriodChange({ index, value: Number(value) })
+                      )
+                    }
+                    onDelete={() => dispatch(deleteOther(index))}
+                  />
+                </span>
+              </div>
+            ))}
+            {other_list.length > 0 && (
+              <button
+                style={{ width: 200, marginTop: 5 }}
+                onClick={() => dispatch(addOther())}
+              >
+                Add Other Income
+              </button>
+            )}
+
+            {/* ------Any loans------ */}
+            <Label text="Do you have any loans?" />
+            <span>
+              <Button
+                title=" Yes "
+                name="radios3"
+                onClick={() => {
+                  console.log("addLoan");
+                  dispatch(addLoan());
+                }}
+                style={{ marginRight: DEFAULTS.btnSpacing }}
+              />
+              <Button
+                title=" No "
+                name="radios3"
+                onClick={() => {
+                  dispatch(deleteAllLoan());
+                }}
+                checked
+              />
+            </span>
+            {loan_list.map((row, index) => (
+              <div key={index}>
+                <Label text={"Loan #" + (index + 1)} />
+                <span>
+                  <LoanField
+                    onChange={(value: string) =>
+                      dispatch(loanChange({ index, value: Number(value) }))
+                    }
+                    onDelete={() => dispatch(deleteLoan(index))}
+                  />
+                </span>
+              </div>
+            ))}
+            {loan_list.length > 0 && (
+              <button
+                style={{ width: 200, marginTop: 5 }}
+                onClick={() => dispatch(addLoan())}
+              >
+                Add Loan
+              </button>
+            )}
+
+            {/* ------Any credit card------ */}
+            <Label text="Do you have any credit cards?" />
+            <span>
+              <Button
+                title="  Yes  "
+                name="radios4"
+                onClick={() => {
+                  dispatch(addCredit());
+                }}
+                style={{ marginRight: DEFAULTS.btnSpacing }}
+              />
+              <Button
+                title="  No  "
+                name="radios4"
+                onClick={() => {
+                  dispatch(deleteAllCredit());
+                }}
+                checked
+              />
+            </span>
+            {credit_list.map((row, index) => (
+              <div key={index}>
+                <Label text={"Credit #" + (index + 1)} />
+                <span>
+                  <LoanField
+                    onChange={(value: string) =>
+                      dispatch(creditChange({ index, value: Number(value) }))
+                    }
+                    onDelete={() => dispatch(deleteCredit(index))}
+                  />
+                </span>
+              </div>
+            ))}
+            {credit_list.length > 0 && (
+              <button
+                style={{ width: 200, marginTop: 5 }}
+                onClick={() => dispatch(addCredit())}
+              >
+                Add credit card
+              </button>
+            )}
+
+            {/* ------How much deposit------ */}
+            <Label text="How much deposite do you have?" />
+            <span>
+              <DepositField
+                onChange={(val: string) => dispatch(depositChange(Number(val)))}
+              />
             </span>
           </FormContainer>
           <CalculationContainer>
-            <h2>Result</h2>
             <Output />
           </CalculationContainer>
         </section>
@@ -76,7 +261,7 @@ const Container = styled.section`
 `;
 
 const Content = styled.div`
-  width: 1200px;
+  width: ${DEFAULTS.contentWidth};
 `;
 
 const FormContainer = styled.div`
@@ -92,5 +277,5 @@ const FormContainer = styled.div`
 const CalculationContainer = styled.div`
   border: 1px dashed green;
   width: 40%;
-  padding: 20px;
+  padding: 10px;
 `;
